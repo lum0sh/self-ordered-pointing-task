7c9267152096d215e1f00845e18dbea24c255c2c
dexhrestha <dipesh.shrestha009@gmail.com>
2025-07-23 17:14:54 +0200

    Updated form for picture completion experiment

--- pages/Picture Completion Experiment.py
+++ pages/Picture Completion Experiment.py
@@ -11,9 +11,14 @@ st.title('Experiment')
 # Input for participant ID
 username = st.text_input('Enter participant id', max_chars=8)
 
-# Button to submit the ID
+# Button to submit and redirect
 if st.button("Submit"):
     if username:
-        st.link_button("Go to experiment", experiment_URL + username)
+        redirect_url = experiment_URL + username
+        # Inject JavaScript to redirect
+        st.markdown(f"""
+            <meta http-equiv="refresh" content="0; url={redirect_url}">
+            <p>Redirecting to <a href="{redirect_url}">{redirect_url}</a>...</p>
+        """, unsafe_allow_html=True)
     else:
         st.warning("Please enter a participant ID.")
