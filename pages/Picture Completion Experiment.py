ef966b5a91151dc39fb92ff34840a57b5574d42d
dexhrestha <dipesh.shrestha009@gmail.com>
2025-07-23 17:13:10 +0200

    Updated form for picture completion experiment

--- pages/Picture Completion Experiment.py
+++ pages/Picture Completion Experiment.py
@@ -1,13 +1,19 @@
 import streamlit as st
- 
+
 st.set_page_config(page_title="Picture Completion")
+
 project_name = 'picompletion'
 experiment_URL = "https://picturecompletion.web.app/?user="
-# Simulating data for the behavioral task
-# participant_ids = get_participant_ids(project_name)
 
 # Title of the app
 st.title('Experiment')
-username = st.text_input('Enter participant id',max_chars=8)
-st.link_button("Go to experiment",experiment_URL+username)
-    
+
+# Input for participant ID
+username = st.text_input('Enter participant id', max_chars=8)
+
+# Button to submit the ID
+if st.button("Submit"):
+    if username:
+        st.link_button("Go to experiment", experiment_URL + username)
+    else:
+        st.warning("Please enter a participant ID.")
