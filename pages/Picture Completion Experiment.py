d1cc92e4fc4c55f2425ee5b90b327964c421442d
dexhrestha <dipesh.shrestha009@gmail.com>
2025-07-23 17:15:58 +0200

    Updated form for picture completion experiment

--- pages/Picture Completion Experiment.py
+++ pages/Picture Completion Experiment.py
@@ -11,14 +11,15 @@ st.title('Experiment')
 # Input for participant ID
 username = st.text_input('Enter participant id', max_chars=8)
 
-# Button to submit and redirect
+# Button to trigger the redirect in a new tab
 if st.button("Submit"):
     if username:
         redirect_url = experiment_URL + username
-        # Inject JavaScript to redirect
         st.markdown(f"""
-            <meta http-equiv="refresh" content="0; url={redirect_url}">
-            <p>Redirecting to <a href="{redirect_url}">{redirect_url}</a>...</p>
+            <script>
+                window.open("{redirect_url}", "_blank");
+            </script>
+            <p>Opening experiment in a new tab: <a href="{redirect_url}" target="_blank">{redirect_url}</a></p>
         """, unsafe_allow_html=True)
     else:
         st.warning("Please enter a participant ID.")
